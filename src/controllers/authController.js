04420894ef34385f4e0900ae16cde3aa101a4b95
Guilherme-tr <gui.trujilo.wecomp@gmail.com>
2022-04-12 21:05:12 -0300

    create router for POST /authenticate user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,33 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    try{
-        if (await User.findOne({type: 'email' })){
-            return res.status(400).send({ error: "User already exists" });
-        }
-            
+    try{       
         const user = await User.create(req.body);
         return res.send({user})
     }catch(err){
         return res.status(400).send({error: 'Registration failed'});
     }
 });
+
+router.post('/authenticate', async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(400).send({ error: 'User not found'});
+    }
+
+    if(!await bcrypt.compare(password, user.password)){
+        return res.status(400).send({ error: 'Invalid password'});
+    }
+
+    res.send({user});
+})
 
 module.exports = app => app.use('/auth', router);
