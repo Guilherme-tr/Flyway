d2b67b7cb178961323b39164cf0f714ae1989fe7
Guilherme-tr <gui.trujilo.wecomp@gmail.com>
2022-04-19 21:45:07 -0300

    Password recovery with NodeMailer

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const porta = 8080;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.status(200).send("Ta funcionando");
+});
+
+require('./app/controllers/index')(app);
+
+app.listen(porta);
